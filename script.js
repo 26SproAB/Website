77eb65703f1a6d77ccfa126cb57c0d5a43d5b0d4
26SproAB <168399849+26SproAB@users.noreply.github.com>
2024-06-21 21:19:55 +1000

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const aflScorer = document.querySelector("#afl-scorer");
+const guessTheNumber = document.querySelector("#guess-the-number");
+const diceRoller = document.querySelector("#dice-roller");
+
+function goToPage(page){
+    window.location.replace(page);
+}
+
+aflScorer.onclick = () => { goToPage("https://26sproab.github.io/Website/afl-scorer")};
+guessTheNumber.onclick = () => { goToPage("https://26sproab.github.io/Website/guess-the-number")};
+diceRoller.onclick = () => { goToPage("https://26sproab.github.io/Website/dice-roller")};
