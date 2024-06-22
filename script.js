d99850d624ca908e8e332ccce7f37ac13695597f
26SproAB <168399849+26SproAB@users.noreply.github.com>
2024-06-22 17:49:23 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const guessTheNumber = document.querySelector("#guess-the-number");
 const diceRoller = document.querySelector("#dice-roller");
 
 function goToPage(page){
-    window.location.replace(page);
+    window.location.replace("page");
 }
 
 aflScorer.onclick = () => { goToPage("https://26sproab.github.io/Website/afl-scorer")};
