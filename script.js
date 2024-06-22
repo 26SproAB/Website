2b97be8d9ab1da83d7fe199dc4278b12bac96acb
26SproAB <168399849+26SproAB@users.noreply.github.com>
2024-06-22 17:51:51 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const aflScorer = document.querySelector("#afl-scorer");
 const guessTheNumber = document.querySelector("#guess-the-number");
 const diceRoller = document.querySelector("#dice-roller");
 
-function goToPage(page){
-    window.location.replace("page");
+function goToPageScorer(){
+    window.location.replace("https://26sproab.github.io/Website/afl-scorer");
 }
 
-aflScorer.onclick = () => { goToPage("https://26sproab.github.io/Website/afl-scorer")};
-guessTheNumber.onclick = () => { goToPage("https://26sproab.github.io/Website/guess-the-number")};
-diceRoller.onclick = () => { goToPage("https://26sproab.github.io/Website/dice-roller")};
+function goToPageGTN(){
+    window.location.replace("https://26sproab.github.io/Website/guess-the-number");
+}
+
+function goToPageRoller(){
+    window.location.replace("https://26sproab.github.io/Website/dice-roller");
+}
+
+
+aflScorer.onclick = goToPageScorer;
+guessTheNumber.onclick = goToPageGTN;
+diceRoller.onclick = goToPageRoller;
