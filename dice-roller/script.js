d04ca2589aefc6da9e54eec91db1f722e66334df
26SproAB <168399849+26SproAB@users.noreply.github.com>
2024-06-23 12:49:42 +1000

    Update script.js

--- dice-roller/script.js
+++ dice-roller/script.js
@@ -1,21 +1,20 @@
 const rollButton = document.querySelector("#roll-button");
-const die1 = document.querySelector("#die1");
-const die2 = document.querySelector("#die2");
+const die1 = document.querySelector("#dice-one");
+const die2 = document.querySelector("#dice-two");
 
-function dieRoll1(){
-  var num1 = Math.floor(Math.random()* (6+1));
+function dieRoll1() {
+  var num1 = Math.floor(Math.random() * 6) + 1;
   return num1;
-  }
+}
 
-function dieRoll2(){
-  var num2 = Math.floor(Math.random()* (6+1));
+function dieRoll2() {
+  var num2 = Math.floor(Math.random() * 6) + 1; 
   return num2;
-  } 
+}
 
-function changeDieFace(){
+function changeDieFace() {
   die1.src = dieRoll1() + ".png";
   die2.src = dieRoll2() + ".png";
 }
-
 
 rollButton.onclick = changeDieFace;
